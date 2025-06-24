6d5c87029dac33a9262b6c086e50a5bbdea49959
gargharshit765@gmail.com <gargharshit765@gmail.com>
2025-06-24 19:34:03 +0530

    Logic

--- DSA_Practice.cpp
+++ DSA_Practice.cpp
@@ -326,3 +326,19 @@ using namespace std;
 
 
 
+// Find Character Lowercase or Uppercase
+int main() {
+    char ch;
+    cout<<"Enter Character : ";
+    cin>>ch;
+
+    if(ch>='a'&&ch<='z'){
+        cout<<"LowerCase";
+    } else {
+        cout<<"UpperCase";
+    }
+
+    return 0;
+}
+
+
