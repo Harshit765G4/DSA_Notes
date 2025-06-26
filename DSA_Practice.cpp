52e81aa5620472109b442fe5dfb95206b4d968e5
gargharshit765@gmail.com <gargharshit765@gmail.com>
2025-06-26 19:47:26 +0530

    Pattern Logic

--- DSA_Practice.cpp
+++ DSA_Practice.cpp
@@ -668,3 +668,84 @@ using namespace std;
 // }
 
 
+
+
+// // 1 
+// // 1 2
+// // 1 2 3
+// // 1 2 3 4
+
+// int main(){
+//     int n = 4;
+//     for(int i = 0; i < n; i++){
+//         for(int j = 1; j <= i+1 ; j++){
+//             cout<<j<<" ";
+//         }
+//         cout<<endl;
+//     }
+// }
+
+
+
+
+
+// // 1 
+// // 2 1
+// // 3 2 1
+// // 4 3 2 1
+
+// int main(){
+//     int n = 4;
+//     for(int i=0;i<n;i++){
+//         for (int j=i+1;j>=1;j--){
+//             cout<<j<<" ";
+//         }
+//         cout<<endl;
+//     }
+// }
+
+
+
+
+
+
+
+//Floyd's Triangle pattern
+
+// // 1 
+// // 2 3
+// // 4 5 6
+// // 7 8 9 10
+
+// int main(){
+//     int n = 4;
+//     int num = 1;
+//     for(int i = 0; i < n; i++){
+//         for(int j = 0; j < i+1 ; j++){
+//             cout<<num<<" ";
+//             num++;
+//         }
+//         cout<<endl;
+//     }
+// }
+
+
+
+
+
+// // A 
+// // B C
+// // D E F
+// // G H I J
+
+// int main(){
+//     int n = 4;
+//     char ch=65;
+//     for(int i=0;i<n;i++){
+//         for(int j=0;j<i+1;j++){
+//             cout<<ch<<" ";
+//             ch++;
+//         }
+//         cout<<endl;
+//     }
+// }
