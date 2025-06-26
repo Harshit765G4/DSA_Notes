225f8a4c397d2852588b6e8d7eb48d372e446181
gargharshit765@gmail.com <gargharshit765@gmail.com>
2025-06-26 18:43:02 +0530

    Pattern Logic

--- DSA_Practice.cpp
+++ DSA_Practice.cpp
@@ -610,3 +610,61 @@ using namespace std;
 
 
 
+
+// // * 
+// // * *
+// // * * *
+// // * * * *
+
+// int main() {
+//     int n=4;
+//     for(int i = 0;i<n;i++){
+//         for(int j=0;j<(i+1);j++){
+//             cout<<"* ";
+//         }
+//         cout<<endl;
+//     }
+// }
+
+
+
+
+
+// // 1 
+// // 2 2 
+// // 3 3 3 
+// // 4 4 4 4
+
+// int main() {
+//     int n=4;
+//     for(int i = 0;i<n;i++){
+//         for(int j=0;j<(i+1);j++){
+//             cout<<i+1<<" ";
+//         }
+//         cout<<endl;
+//     }
+// }
+
+
+
+
+
+// // A 
+// // B B
+// // C C C
+// // D D D D
+// // E E E E E
+
+// int main(){
+//     int n=5;
+//     char ch=65;
+//     for(int i=0;i<n;i++){
+//         for(int j=0;j<i+1;j++){
+//             cout<<ch<<" ";
+//         }
+//         ch++;
+//         cout<<endl;
+//     }
+// }
+
+
