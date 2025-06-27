a076d31f0e1cc9be51d53eef679efcbd615cfcc5
gargharshit765@gmail.com <gargharshit765@gmail.com>
2025-06-27 18:10:15 +0530

    Pattern logic

--- DSA_Practice.cpp
+++ DSA_Practice.cpp
@@ -749,3 +749,195 @@ using namespace std;
 //         cout<<endl;
 //     }
 // }
+
+
+
+
+// // A 
+// // B A
+// // C B A
+// // D C B A
+
+// int main(){
+//     int n = 4;
+//     for(int i = 0; i < n; i++){
+//         char ch = 'A'+i;
+//         for(int j = 0; j<i+1; j++){
+//             cout<<(char)(ch-j)<<" ";
+//         }
+//         cout<<endl;
+//     } 
+//     return 0;
+// }
+
+
+
+
+
+
+// Inverted Triangle Pattern
+
+// Reverse numbers n=4
+
+
+
+
+// // 1111
+// //  222
+// //   33
+// //    4
+
+// int main() {
+//     int n = 4;
+//     for(int i = 0; i < n; i++){
+        // for(int j = 0; j < i ; j++){
+        //     cout<<" ";
+        // }
+        // for(int k=0;k<n-i;k++){
+        //     cout<<i+1;
+        // }
+//         cout<<endl;
+//     }
+//     return 0;
+// }
+
+
+
+
+
+// // AAAA
+// //  BBB
+// //   CC
+// //    D
+
+// int main() {
+//     int n = 4;
+//     char ch='A';
+//     for(int i = 0; i < n; i++){
+        // for(int j = 0; j < i ; j++){
+        //     cout<<" ";
+        // }
+//         for(int k=0;k<n-i;k++){
+//             cout<<ch;
+//         }
+//         ch++;
+//         cout<<endl;
+//     }
+//     return 0;
+// }
+
+
+
+
+
+
+// Pyramid Pattern
+
+
+// //    1
+// //   121
+// //  12321
+// // 1234321
+
+// int main(){
+//     int n = 4;
+//     for(int i=0;i<n;i++){
+//         for(int j=0;j<n-(i+1);j++){
+//             cout<<" ";
+//         }
+//         for(int j=0;j<i+1;j++){
+//             cout<<j+1;
+//         }
+//         for(int j=i;j>0;j--){
+//             cout<<j;
+//         }
+//         cout<<endl;
+//     }
+// }
+
+
+
+
+
+// Hollow Diamond Patter
+
+
+//       * 
+//     *   * 
+//   *       * 
+// *           * 
+//   *       * 
+//     *   * 
+//       * 
+      
+
+
+// My Logic
+
+// int main(){
+//     int n=4;
+//     for(int i=0;i<n;i++){
+//         for(int j=0;j<n-(i+1);j++){
+//             cout<<"  ";
+//         }
+//         cout<<"* ";
+//         if(i!=0){
+//         for(int j=2*i-1;j>0;j--){
+//             cout<<"  ";
+//         }
+//         cout<<"* ";
+//         }
+//         cout<<endl;
+//     }
+//     for(int i=n;i>1;i--){
+//         for(int j=n-i;j>=0;j--){
+//             cout<<"  ";
+//         }
+//         cout<<"* ";
+//         for(int j=i-1;j>1;j--){
+//             cout<<"  ";
+//         }
+//         for(int j=i;j>3;j--){
+//             cout<<"  ";
+//         }
+//         if(n!=i+2){
+//             cout<<"* ";
+//         }
+//         cout<<endl;
+//     }
+// }
+
+
+
+// optimized logic for Hollow Diamond pattern
+
+int main(){
+    int n = 4;
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n - i - 1; j++) {
+            cout << "  ";
+        }
+        cout << "* ";
+        if(i != 0){
+            for(int j = 0; j < 2*i - 1; j++) {
+                cout << "  ";
+            }
+            cout << "* ";
+        }
+        cout << endl;
+    }
+    for(int i = n - 2 ; i >= 0; i--) {
+        for(int j = 0; j < n - i - 1; j++) {
+            cout << "  ";
+        }
+        cout << "* ";
+        if(i != 0){
+            for(int j = 0; j < 2*i - 1; j++) {
+                cout << "  ";
+            }
+            cout << "* ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
