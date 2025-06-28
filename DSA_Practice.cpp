518a8b8c62c87f1b8565001e15a97bd45818208c
gargharshit765@gmail.com <gargharshit765@gmail.com>
2025-06-28 12:04:33 +0530

    Functions

--- DSA_Practice.cpp
+++ DSA_Practice.cpp
@@ -961,30 +961,47 @@ using namespace std;
 // *                    *
 
 
-// int main(){
-//     int n=4;
-//     for(int i=0;i<n;i++){
-//         for(int j=0;j<i+1;j++){
-//             cout<<"*  ";
-//         }
-//         for(int j=0;j<2*(n-i-1);j++){
-//             cout<<"   ";
-//         }
-//         for(int j=0;j<i+1;j++){
-//             cout<<"*  ";
-//         }
-//         cout<<endl;
-//     }
-//     for(int i=n;i>0;i--){
-//         for(int j=0;j<i;j++){
-//             cout<<"*  ";
-//         }
-//         for(int j=2*(n-i);j>0;j--){
-//             cout<<"   ";
-//         }
-//         for(int j=0;j<i;j++){
-//             cout<<"*  ";
-//         }
-//     cout<<endl;
-//     }
+int main(){
+    int n=4;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<i+1;j++){
+            cout<<"*  ";
+        }
+        for(int j=0;j<2*(n-i-1);j++){
+            cout<<"   ";
+        }
+        for(int j=0;j<i+1;j++){
+            cout<<"*  ";
+        }
+        cout<<endl;
+    }
+    for(int i=n;i>0;i--){
+        for(int j=0;j<i;j++){
+            cout<<"*  ";
+        }
+        for(int j=2*(n-i);j>0;j--){
+            cout<<"   ";
+        }
+        for(int j=0;j<i;j++){
+            cout<<"*  ";
+        }
+    cout<<endl;
+    }
+}
+
+
+
+
+
+
+
+
+// Functions
+
+// returnType printHello(){
+//         cout<<"hello";
 // }
+
+
+
+ 
