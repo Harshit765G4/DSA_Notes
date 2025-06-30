3ac9d26dd7403f79a82ecef405595fcf9a957987
gargharshit765@gmail.com <gargharshit765@gmail.com>
2025-06-30 20:14:25 +0530

    functionLogic

--- DSA_Practice.cpp
+++ DSA_Practice.cpp
@@ -1272,4 +1272,21 @@ using namespace std;
 
 
 
-
+// Decimal to binary
+double Dec2Bin(int num){
+    double ans = 0;
+    int pow = 1;
+    while(num > 0){
+        int rem = num % 2;
+        num = num / 2;
+        ans += (rem*pow);
+        pow = pow * 10;
+    }
+    return ans;
+}
+int main(){
+    int num;
+    cout<<"Enter num: ";
+    cin>>num;
+    cout<<Dec2Bin(num);
+}
