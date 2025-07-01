e5ac2b38e56a42b240d16a6dea6cf30dea5c59d0
gargharshit765@gmail.com <gargharshit765@gmail.com>
2025-07-01 20:45:53 +0530

    LogicUpdate

--- DSA_Practice.cpp
+++ DSA_Practice.cpp
@@ -1215,17 +1215,17 @@ using namespace std;
 // // WAF to print nth Fibonacci
 
 // int fibonacci(int n){
-   // if(n<=0) return 0;
-   // if(n==1) return 1;
+//    if(n<=0) return 0;
+//    if(n==1) return 1;
 
-   // int a = 0,b = 1, fib;
+//    int a = 0,b = 1, fib;
 
-   // for(int i=2;i<=n;i++){
-   //    fib = a + b;
-   //    a = b;
-   //    b = fib;
-   // }
-   // return b;
+//    for(int i=2;i<=n;i++){
+//       fib = a + b;
+//       a = b;
+//       b = fib;
+//    }
+//    return b;
 // }
 
 // int main(){
