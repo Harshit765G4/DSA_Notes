16937127630eae802f3b0f91dc8025555babd894
gargharshit765@gmail.com <gargharshit765@gmail.com>
2025-07-03 13:59:52 +0530

    Array Input Output

--- Arrays.cpp
+++ Arrays.cpp
@@ -1,7 +1,30 @@
 #include<iostream>
 using namespace std;
 
-int main(){
-   cout<<"hello arrays";
-   return 0;
-}
+
+// // output array
+// int main(){
+//     int marks[5] = {50,65,84,75,69};
+//     for(int i = 0;i<5;i++){
+//       cout<<marks[i]<<endl;
+//     }
+//     cout<<"MarksSize: "<<sizeof(marks)/sizeof(int);
+// }
+
+
+
+
+// // input array
+// int main(){
+//    int size = 5;
+//    int marks[size];
+//    cout<<"Enter Marks: ";
+
+//    // input array 
+//    for(int i = 0; i<size; i++){
+//       cin>>marks[i];
+//    }
+//    for(int i = 0; i<size; i++){
+//       cout<<"Marks: "<<marks[i]<<endl;
+//    }
+// }
