1b47a34c0411eef0450941768fed64510a0caedc
gargharshit765@gmail.com <gargharshit765@gmail.com>
2025-07-03 14:11:49 +0530

    Array Smallest and Largest

--- Arrays.cpp
+++ Arrays.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<climits>
 using namespace std;
 
 
@@ -28,3 +29,34 @@ using namespace std;
 //       cout<<"Marks: "<<marks[i]<<endl;
 //    }
 // }
+
+
+
+
+
+// // Smallest element
+// int main(){
+//    int arr[] = {5,15,22,1,-15,24};
+//    int smallest = INT_MAX;
+//    for(int i=0;i<sizeof(arr);i++){
+//       if(arr[i]<smallest){
+//          smallest = arr[i];
+//       }
+//    }
+//    cout<<smallest;
+// }
+
+
+
+
+// largest Element
+int main(){
+   int arr[] = {5,15,22,1,-15,24};
+   int largest = INT_MIN;
+   for(int i=0;i<sizeof(arr);i++){
+      if(arr[i]>largest){
+         largest = arr[i];
+      }
+   }
+   cout<<largest;
+}
