a483eb0f24be3c0ace8903f00bd6ea817faadb05
gargharshit765@gmail.com <gargharshit765@gmail.com>
2025-07-04 17:49:16 +0530

    arrayLogic

--- Arrays.cpp
+++ Arrays.cpp
@@ -1,4 +1,5 @@
 #include<bits/stdc++.h>
+#include<vector>          //header file for vector in c++
 using namespace std;
 
 
@@ -187,31 +188,23 @@ using namespace std;
 
 
 
-// Reverse an array
-void reverseArray(int arr[], int start, int end) {
-   while(start < end) {
-      swap(arr[start], arr[end]);
-      start++;
-      end--;
-   }
-}
-int main() {
-   int arr[] = {4,2,7,8,1,2,5};
-   int start = 0;
-   int size = sizeof(arr)/sizeof(arr[0]);
-   int end = size - 1;
-   reverseArray(arr, start, end);
-   for(int i = 0; i < size; i++) {
-      cout << arr[i] << " ";
-   }
-   cout << endl;
-   return 0;
-}
-
-
-
-
-
-
-
-
+// // Reverse an array
+// void reverseArray(int arr[], int start, int end) {
+//    while(start < end) {
+//       swap(arr[start], arr[end]);
+//       start++;
+//       end--;
+//    }
+// }
+// int main() {
+//    int arr[] = {4,2,7,8,1,2,5};
+//    int start = 0;
+//    int size = sizeof(arr)/sizeof(arr[0]);
+//    int end = size - 1;
+//    reverseArray(arr, start, end);
+//    for(int i = 0; i < size; i++) {
+//       cout << arr[i] << " ";
+//    }
+//    cout << endl;
+//    return 0;
+// }
