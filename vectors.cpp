71d4e14807fa2f06b73c98dd70ade54dc548be42
gargharshit765@gmail.com <gargharshit765@gmail.com>
2025-07-04 20:11:05 +0530

    Vector size and capacity

--- vectors.cpp
+++ vectors.cpp
@@ -39,26 +39,42 @@ using namespace std;
 
 
 
-// Inserting and Removing an element in a vector
+// // Inserting and Removing an element in a vector
+// // also accessing front and back values
+// int main(){
+//    vector<int> vec;
+//    cout<< "Size: " << vec.size()<<endl;
+//    vec.push_back(25);
+//    vec.push_back(55);
+//    vec.push_back(75);
+//    cout<< "After Push_back: " << vec.size()<<endl;
+//    for(int val : vec){
+//       cout<<val<<endl;
+//    } 
+//    vec.pop_back();
+//    cout<< "After Pop_back: " << vec.size()<<endl;
+//    for(int val : vec){
+//       cout<<val<<endl;
+//    } 
+//    cout<<endl;
+//    cout<<endl;
+//    cout<<"Front value: "<<vec.front()<<endl;
+//    cout<<"Back Value: "<<vec.back()<<endl;
+//    cout<<"Accessing at Index 1: "<<vec[1];
+//    return 0;
+// }
+
+
+
+
+
+// size and capacity
+
 int main(){
    vector<int> vec;
-   cout<< "Size: " << vec.size()<<endl;
+   vec.push_back(5);
+   vec.push_back(15);
    vec.push_back(25);
-   vec.push_back(55);
-   vec.push_back(75);
-   cout<< "After Push_back: " << vec.size()<<endl;
-   for(int val : vec){
-      cout<<val<<endl;
-   } 
-   vec.pop_back();
-   cout<< "After Pop_back: " << vec.size()<<endl;
-   for(int val : vec){
-      cout<<val<<endl;
-   } 
-   return 0;
+   cout<<"Size: "<<vec.size()<<endl;
+   cout<<"Capacity: "<<vec.capacity()<<endl;
 }
-
-
-
-
-
