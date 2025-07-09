57f7582045ac3151381ff0b25ec8b35b785cab9e
gargharshit765@gmail.com <gargharshit765@gmail.com>
2025-07-09 23:55:51 +0530

    Pointers

--- pointers.cpp
+++ pointers.cpp
@@ -1,6 +1,8 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+
+// // Memory Address
 // int main(){
 //    int a = 10;
 //    cout<< &a << endl;
@@ -8,11 +10,37 @@ using namespace std;
 // }
 
 
+// // pointers
+// int main(){
+//    int x = 10;
+//    int* ptr = &x;
+//    cout<< ptr << endl;
+//    cout<< &x << endl;
+//    cout<< &ptr << endl;
 
-int main(){
-   int x = 10;
-   int* ptr = &x;
-   cout<< ptr << endl;
-   cout<< &x << endl;
-   return 0;
-}
+
+//    // stores the adress of another pointer
+//    int** ptr2 = &ptr;
+//    cout << ptr2 << endl;
+//    return 0;
+// }
+
+
+
+
+
+// // dereferencing operator
+// int main(){
+//    int x = 10;
+//    int* ptr = &x;
+//    cout<< *ptr << endl;
+//    return 0;
+// }
+
+
+
+// // Null Pointer
+// int main(){
+//    int* ptr = nullptr;
+//    return 0;
+// }
