f1766d38897e8aa7f20b98fd32b22088adf51a88
gargharshit765@gmail.com <gargharshit765@gmail.com>
2025-07-10 23:08:30 +0530

    Array pointer

--- pointers.cpp
+++ pointers.cpp
@@ -50,15 +50,22 @@ using namespace std;
 
 
 
-// pass by reference
-void swap(int &x, int &y) {
-    int temp = x;
-    x = y;
-    y = temp;
-}
+// // pass by reference
+// void swap(int &x, int &y) {
+//     int temp = x;
+//     x = y;
+//     y = temp;
+// }
 
-int main() {
-    int a = 10, b = 20;
-    swap(a, b);
-    cout << a << " " << b;  // Output: 20 10
-}
+// int main() {
+//     int a = 10, b = 20;
+//     swap(a, b);
+//     cout << a << " " << b;  // Output: 20 10
+// }
+
+
+
+
+
+
+// Array pointer
