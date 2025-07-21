bb49125ad698bb18bc628f6b0cce5026e8c56776
gargharshit765@gmail.com <gargharshit765@gmail.com>
2025-07-21 23:56:04 +0530

    LeetCode Problem 14: Longest Common Prefix

--- DSA_Practice.cpp
+++ DSA_Practice.cpp
@@ -1917,3 +1917,29 @@
 //     cout << "Capitalized: " << str << endl;
 //     return 0;
 // }
+
+
+
+
+
+
+
+
+
+//  LeetCode Problem 14: Longest Common Prefix 
+
+
+// string longestCommonPrefix(vector<string>& strs) {
+//         if (strs.empty()) return "";
+
+//         string prefix = strs[0];
+
+//         for (int i = 1; i < strs.size(); ++i) {
+//             while (strs[i].find(prefix) != 0) {
+//                 prefix = prefix.substr(0, prefix.length() - 1);
+//                 if (prefix.empty()) return "";
+//             }
+//         }
+
+//         return prefix;
+//     }
