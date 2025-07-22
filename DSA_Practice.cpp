1c898c7cc09c27ee8f59d2bdde6e81c87e913421
HARSHIT GARG <141812532+Harshit765G4@users.noreply.github.com>
2025-07-22 23:38:43 +0530

    Update DSA_Practice.cpp

--- DSA_Practice.cpp
+++ DSA_Practice.cpp
@@ -2019,3 +2019,25 @@
 //         return -1;
 //     }
 // };
+
+
+
+
+
+
+
+// // 283. Move Zeros
+
+// class Solution {
+// public:
+//     void moveZeroes(vector<int>& nums) {
+//         int lastNonZeroIndex = 0;
+        
+//         for (int i = 0; i < nums.size(); i++) {
+//             if (nums[i] != 0) {
+//                 swap(nums[i], nums[lastNonZeroIndex]);
+//                 lastNonZeroIndex++;
+//             }
+//         }
+//     }
+// };
