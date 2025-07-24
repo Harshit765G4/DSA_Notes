8cd70c03dd31a877bd13c2bb40d089abd3a2ea45
Hey_Nerd <gargharshit765@gmail.com>
2025-07-24 23:13:20 +0530

    Two Sum II – Input Array is Sorted (LeetCode #167)

--- DSA_Practice.cpp
+++ DSA_Practice.cpp
@@ -2169,3 +2169,38 @@
 //         return {};
 //     }
 // };
+
+
+
+
+
+
+
+
+
+// // Two Sum II – Input Array is Sorted (LeetCode #167)
+
+// class Solution {
+// public:
+//     vector<int> twoSum(vector<int>& numbers, int target) {
+//         int left = 0, right = numbers.size() - 1;
+//         while (left < right) {
+//             int sum = numbers[left] + numbers[right];
+//             if (sum == target) return {left + 1, right + 1};
+//             else if (sum < target) ++left;
+//             else --right;
+//         }
+//         return {};
+//     }
+// };
+
+
+
+
+
+
+
+
+
+
+
