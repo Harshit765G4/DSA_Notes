0b48a2a59f0cafc6d18fc345004bc43590353af1
Hey_Nerd <gargharshit765@gmail.com>
2025-07-24 23:12:23 +0530

    Two Sum

--- DSA_Practice.cpp
+++ DSA_Practice.cpp
@@ -2144,3 +2144,28 @@
 //         }
 //     }
 // };
+
+
+
+
+
+
+
+
+
+
+// // Two Sum (LeetCode #1)
+// class Solution {
+// public:
+//     vector<int> twoSum(vector<int>& nums, int target) {
+//         unordered_map<int, int> mp; // num -> index
+//         for (int i = 0; i < nums.size(); ++i) {
+//             int complement = target - nums[i];
+//             if (mp.count(complement)) {
+//                 return {mp[complement], i};
+//             }
+//             mp[nums[i]] = i;
+//         }
+//         return {};
+//     }
+// };
