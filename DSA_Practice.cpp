ebfcac5d90ace8f1d5b9825fc9e272fdff68d6e4
Hey_Nerd <gargharshit765@gmail.com>
2025-07-25 22:55:59 +0530

    Problem: 268. Missing Number

--- DSA_Practice.cpp
+++ DSA_Practice.cpp
@@ -2231,3 +2231,30 @@
 //     }
 // };
 
+
+
+
+
+
+
+
+
+
+// // Problem: 268. Missing Number
+
+// class Solution {
+// public:
+//     int missingNumber(vector<int>& nums) {
+//         int n = nums.size();
+//         int xorAll = 0, xorNums = 0;
+
+//         for (int i = 0; i <= n; ++i) {
+//             xorAll ^= i;          // XOR of 0 to n
+//         }
+//         for (int num : nums) {
+//             xorNums ^= num;       // XOR of all elements in array
+//         }
+
+//         return xorAll ^ xorNums;  // Missing number
+//     }
+// };
