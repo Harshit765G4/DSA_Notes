1e78a443ad7bd37c5d8f524966c204eebc8d7b35
Hey_Nerd <gargharshit765@gmail.com>
2025-07-25 22:58:10 +0530

    41. First Missing Positive

--- DSA_Practice.cpp
+++ DSA_Practice.cpp
@@ -2282,3 +2282,30 @@
 //         return result;
 //     }
 // };
+
+
+
+
+
+
+
+
+
+// // 41. First Missing Positive
+
+// class Solution {
+// public:
+//     int firstMissingPositive(vector<int>& nums) {
+//         int n = nums.size();
+//         for (int i = 0; i < n; ++i) {
+//             while (nums[i] > 0 && nums[i] <= n && nums[i] != nums[nums[i] - 1]) {
+//                 swap(nums[i], nums[nums[i] - 1]);  // Put nums[i] at correct index
+//             }
+//         }
+
+//         for (int i = 0; i < n; ++i) {
+//             if (nums[i] != i + 1) return i + 1;
+//         }
+//         return n + 1;
+//     }
+// };
