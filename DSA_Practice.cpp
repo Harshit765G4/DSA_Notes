62776d0ffbc2c09010b3a1310242d822b1f06cfa
Hey_Nerd <gargharshit765@gmail.com>
2025-07-26 23:54:54 +0530

    LeetCode 28: Find the Index of the First Occurrence in a String

--- DSA_Practice.cpp
+++ DSA_Practice.cpp
@@ -2364,3 +2364,27 @@
 //         return slow;
 //     }
 // };
+
+
+
+
+
+
+
+
+
+// // LeetCode 28: Find the Index of the First Occurrence in a String
+
+// class Solution {
+// public:
+//     int strStr(string haystack, string needle) {
+//         int n = haystack.length();
+//         int m = needle.length();
+        
+//         for (int i = 0; i <= n - m; ++i) {
+//             if (haystack.substr(i, m) == needle)
+//                 return i;
+//         }
+//         return -1;
+//     }
+// };
