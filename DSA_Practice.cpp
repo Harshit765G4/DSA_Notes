b448b9c9444818774e40f87ef5d64f81e2830d13
Hey_Nerd <gargharshit765@gmail.com>
2025-07-31 23:52:42 +0530

    205. Isomorphic Strings

--- DSA_Practice.cpp
+++ DSA_Practice.cpp
@@ -2815,3 +2815,29 @@
 
 
 
+
+
+
+
+
+// // 205. Isomorphic Strings
+
+// class Solution {
+// public:
+//     bool isIsomorphic(string s, string t) {
+//         if (s.length() != t.length()) return false;
+
+//         unordered_map<char, char> m1, m2;
+
+//         for (int i = 0; i < s.length(); i++) {
+//             if ((m1.count(s[i]) && m1[s[i]] != t[i]) || 
+//                 (m2.count(t[i]) && m2[t[i]] != s[i]))
+//                 return false;
+
+//             m1[s[i]] = t[i];
+//             m2[t[i]] = s[i];
+//         }
+
+//         return true;
+//     }
+// };
