ec1fd62cb62fcd6b2f03a6d48242489eb15efdc6
Hey_Nerd <gargharshit765@gmail.com>
2025-08-01 23:58:45 +0530

    33. Search in Rotated Sorted Array

--- DSA_Practice.cpp
+++ DSA_Practice.cpp
@@ -2913,3 +2913,42 @@
 //         return left;
 //     }
 // };
+
+
+
+
+
+
+
+
+// // 33. Search in Rotated Sorted Array
+
+// class Solution {
+// public:
+//     int search(vector<int>& nums, int target) {
+//         int left = 0, right = nums.size() - 1;
+
+//         while (left <= right) {
+//             int mid = (left + right) / 2;
+
+//             if (nums[mid] == target) return mid;
+
+//             // Left part is sorted
+//             if (nums[left] <= nums[mid]) {
+//                 if (nums[left] <= target && target < nums[mid])
+//                     right = mid - 1;
+//                 else
+//                     left = mid + 1;
+//             }
+//             // Right part is sorted
+//             else {
+//                 if (nums[mid] < target && target <= nums[right])
+//                     left = mid + 1;
+//                 else
+//                     right = mid - 1;
+//             }
+//         }
+
+//         return -1;
+//     }
+// };
