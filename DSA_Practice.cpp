8f6409d9ca5d3afe55a2a4cfcf53d3d1cb02da90
Hey_Nerd <gargharshit765@gmail.com>
2025-08-01 23:57:18 +0530

    Leetcode Problem 34: Find First and Last Position of Element in Sorted Array.

--- DSA_Practice.cpp
+++ DSA_Practice.cpp
@@ -2841,3 +2841,47 @@
 //         return true;
 //     }
 // };
+
+
+
+
+
+
+
+
+// // Leetcode Problem 34: Find First and Last Position of Element in Sorted Array. 
+
+// class Solution {
+// public:
+//     vector<int> searchRange(vector<int>& nums, int target) {
+//         int first = findBound(nums, target, true);
+//         int last = findBound(nums, target, false);
+//         return {first, last};
+//     }
+    
+//     int findBound(vector<int>& nums, int target, bool findFirst) {
+//         int left = 0, right = nums.size() - 1;
+//         int result = -1;
+        
+//         while (left <= right) {
+//             int mid = left + (right - left) / 2;
+            
+//             if (nums[mid] == target) {
+//                 result = mid;
+//                 // Keep searching on the left (for first) or right (for last)
+//                 if (findFirst)
+//                     right = mid - 1;
+//                 else
+//                     left = mid + 1;
+//             }
+//             else if (nums[mid] < target) {
+//                 left = mid + 1;
+//             }
+//             else {
+//                 right = mid - 1;
+//             }
+//         }
+        
+//         return result;
+//     }
+// };
