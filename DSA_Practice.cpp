e8a2029f1ced161e50b2333f0817f52c12d00acb
Hey_Nerd <gargharshit765@gmail.com>
2025-08-03 23:28:27 +0530

    22. Generate Parentheses

--- DSA_Practice.cpp
+++ DSA_Practice.cpp
@@ -3093,3 +3093,34 @@
 //         }
 //     }
 // };
+
+
+
+
+
+
+
+
+
+// // 22. Generate Parentheses
+
+// class Solution {
+// public:
+//     vector<string> generateParenthesis(int n) {
+//         vector<string> result;
+//         backtrack("", 0, 0, n, result);
+//         return result;
+//     }
+
+//     void backtrack(string current, int open, int close, int max, vector<string>& result) {
+//         if (current.length() == max * 2) {
+//             result.push_back(current);
+//             return;
+//         }
+
+//         if (open < max)
+//             backtrack(current + "(", open + 1, close, max, result);
+//         if (close < open)
+//             backtrack(current + ")", open, close + 1, max, result);
+//     }
+// };
