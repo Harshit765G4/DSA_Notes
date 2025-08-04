953b0fb1261358dc9ab7a31c2736e1f83c1652c2
Hey_Nerd <gargharshit765@gmail.com>
2025-08-04 23:48:35 +0530

    220. Contains Duplicate III

--- DSA_Practice.cpp
+++ DSA_Practice.cpp
@@ -3150,3 +3150,37 @@
 //         return false;
 //     }
 // };
+
+
+
+
+
+
+
+
+
+
+// // 220. Contains Duplicate III
+
+// class Solution {
+// public:
+//     bool containsNearbyAlmostDuplicate(vector<int>& nums, int k, int t) {
+//         if (k <= 0 || t < 0) return false;
+        
+//         set<long long> window;
+
+//         for (int i = 0; i < nums.size(); ++i) {
+//             auto pos = window.lower_bound((long long)nums[i] - t);
+            
+//             if (pos != window.end() && *pos <= (long long)nums[i] + t)
+//                 return true;
+
+//             window.insert(nums[i]);
+
+//             if (i >= k)
+//                 window.erase(nums[i - k]);
+//         }
+
+//         return false;
+//     }
+// };
