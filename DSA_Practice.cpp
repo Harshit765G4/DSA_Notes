a04029a3a64629d688b713dc9acc568f6478b3ca
Hey_Nerd <gargharshit765@gmail.com>
2025-08-04 23:47:28 +0530

    219. Contains Duplicate II

--- DSA_Practice.cpp
+++ DSA_Practice.cpp
@@ -3124,3 +3124,29 @@
 //             backtrack(current + ")", open, close + 1, max, result);
 //     }
 // };
+
+
+
+
+
+
+
+
+
+// //219. Contains Duplicate II
+
+// class Solution {
+// public:
+//     bool containsNearbyDuplicate(vector<int>& nums, int k) {
+//         unordered_map<int, int> seen;
+
+//         for (int i = 0; i < nums.size(); ++i) {
+//             if (seen.count(nums[i]) && i - seen[nums[i]] <= k) {
+//                 return true;
+//             }
+//             seen[nums[i]] = i;
+//         }
+
+//         return false;
+//     }
+// };
