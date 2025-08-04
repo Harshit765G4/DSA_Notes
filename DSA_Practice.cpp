b24ddf39fcac34dd44d3ffd6bde3f3a1a1821bca
Hey_Nerd <gargharshit765@gmail.com>
2025-08-04 23:49:26 +0530

    128. Longest Consecutive Sequence

--- DSA_Practice.cpp
+++ DSA_Practice.cpp
@@ -3184,3 +3184,37 @@
 //         return false;
 //     }
 // };
+
+
+
+
+
+
+
+
+
+// // 128. Longest Consecutive Sequence
+
+// class Solution {
+// public:
+//     int longestConsecutive(vector<int>& nums) {
+//         unordered_set<int> numSet(nums.begin(), nums.end());
+//         int longest = 0;
+
+//         for (int num : numSet) {
+//             if (!numSet.count(num - 1)) {
+//                 int current = num;
+//                 int length = 1;
+
+//                 while (numSet.count(current + 1)) {
+//                     current++;
+//                     length++;
+//                 }
+
+//                 longest = max(longest, length);
+//             }
+//         }
+
+//         return longest;
+//     }
+// };
