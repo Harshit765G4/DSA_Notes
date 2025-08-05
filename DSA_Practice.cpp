17c2660a5c85533e23fcb0e15ca5febd04beb27f
Hey_Nerd <gargharshit765@gmail.com>
2025-08-05 23:55:10 +0530

    120. Triangle (Minimum Path Sum)

--- DSA_Practice.cpp
+++ DSA_Practice.cpp
@@ -3271,3 +3271,29 @@
 //         return row;
 //     }
 // };
+
+
+
+
+
+
+
+
+
+// // 120. Triangle (Minimum Path Sum)
+
+// class Solution {
+// public:
+//     int minimumTotal(vector<vector<int>>& triangle) {
+//         int n = triangle.size();
+//         vector<int> dp = triangle[n - 1];
+
+//         for (int i = n - 2; i >= 0; --i) {
+//             for (int j = 0; j < triangle[i].size(); ++j) {
+//                 dp[j] = triangle[i][j] + min(dp[j], dp[j + 1]);
+//             }
+//         }
+
+//         return dp[0];
+//     }
+// };
