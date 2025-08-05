6de1460a8f5eaaa5eab0c6aa612c5114ea8dc35c
Hey_Nerd <gargharshit765@gmail.com>
2025-08-05 23:54:16 +0530

    Leetcode Problem 118: Pascal's Triangle

--- DSA_Practice.cpp
+++ DSA_Practice.cpp
@@ -3218,3 +3218,31 @@
 //         return longest;
 //     }
 // };
+
+
+
+
+
+
+
+// // Leetcode Problem 118: Pascal's Triangle
+
+// class Solution {
+// public:
+//     vector<vector<int>> generate(int numRows) {
+//         vector<vector<int>> triangle;
+
+//         for (int i = 0; i < numRows; ++i) {
+//             vector<int> row(i + 1, 1); // Initialize all elements as 1
+
+//             for (int j = 1; j < i; ++j) {
+//                 // Each element = sum of the two elements above it
+//                 row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j];
+//             }
+
+//             triangle.push_back(row);
+//         }
+
+//         return triangle;
+//     }
+// };
