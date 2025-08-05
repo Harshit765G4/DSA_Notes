3437d8409eb37f31a770570096d3fac2fb7a3ea1
Hey_Nerd <gargharshit765@gmail.com>
2025-08-05 23:56:24 +0530

    66. Plus One

--- DSA_Practice.cpp
+++ DSA_Practice.cpp
@@ -3297,3 +3297,30 @@
 //         return dp[0];
 //     }
 // };
+
+
+
+
+
+
+
+
+// // 66. Plus One
+
+// class Solution {
+// public:
+//     vector<int> plusOne(vector<int>& digits) {
+//         int n = digits.size();
+
+//         for (int i = n - 1; i >= 0; --i) {
+//             if (digits[i] < 9) {
+//                 digits[i]++;
+//                 return digits;
+//             }
+//             digits[i] = 0;
+//         }
+
+//         digits.insert(digits.begin(), 1);
+//         return digits;
+//     }
+// };
