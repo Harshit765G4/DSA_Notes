74655105b94732df16b6391877cc9497d86a7340
Hey_Nerd <gargharshit765@gmail.com>
2025-08-07 23:22:17 +0530

    206. Reverse Linked List

--- DSA_Practice.cpp
+++ DSA_Practice.cpp
@@ -3368,5 +3368,21 @@
 
 
 
+// //  206. Reverse Linked List
 
+// class Solution {
+// public:
+//     ListNode* reverseList(ListNode* head) {
+//         ListNode* prev = nullptr;
+//         ListNode* curr = head;
 
+//         while (curr != nullptr) {
+//             ListNode* nextTemp = curr->next;
+//             curr->next = prev;
+//             prev = curr;
+//             curr = nextTemp;
+//         }
+
+//         return prev;
+//     }
+// };
