6f06ecbf0d10725f7c7e02e84f18fc91116169d4
Hey_Nerd <gargharshit765@gmail.com>
2025-08-10 22:10:25 +0530

    LeetCode 148. Sort List

--- DSA_Practice.cpp
+++ DSA_Practice.cpp
@@ -3443,3 +3443,68 @@
 //         return dummy->next;
 //     }
 // };
+
+
+
+
+
+
+
+
+// // LeetCode 148. Sort List
+
+// /**
+//  * Definition for singly-linked list.
+//  * struct ListNode {
+//  *     int val;
+//  *     ListNode *next;
+//  *     ListNode() : val(0), next(nullptr) {}
+//  *     ListNode(int x) : val(x), next(nullptr) {}
+//  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+//  * };
+//  */
+// class Solution {
+// public:
+//     ListNode* sortList(ListNode* head) {
+//         // Base case
+//         if (!head || !head->next) return head;
+
+//         // Find middle
+//         ListNode* slow = head;
+//         ListNode* fast = head;
+//         ListNode* prev = nullptr;
+//         while (fast && fast->next) {
+//             prev = slow;
+//             slow = slow->next;
+//             fast = fast->next->next;
+//         }
+//         prev->next = nullptr; // Split list into two halves
+
+//         // Sort each half
+//         ListNode* left = sortList(head);
+//         ListNode* right = sortList(slow);
+
+//         // Merge sorted halves
+//         return merge(left, right);
+//     }
+
+//     // Merge two sorted linked lists
+//     ListNode* merge(ListNode* l1, ListNode* l2) {
+//         ListNode dummy(0);
+//         ListNode* tail = &dummy;
+
+//         while (l1 && l2) {
+//             if (l1->val < l2->val) {
+//                 tail->next = l1;
+//                 l1 = l1->next;
+//             } else {
+//                 tail->next = l2;
+//                 l2 = l2->next;
+//             }
+//             tail = tail->next;
+//         }
+
+//         tail->next = l1 ? l1 : l2;
+//         return dummy.next;
+//     }
+// };
