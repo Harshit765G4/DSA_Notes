f7535e860dc8481f5e03cc4eccfdf554f269e574
Hey_Nerd <gargharshit765@gmail.com>
2025-08-11 23:50:28 +0530

    practice

--- DSA_Practice.cpp
+++ DSA_Practice.cpp
@@ -3638,3 +3638,30 @@
 //     }
 //     cout << endl;
 // }
+
+
+
+
+
+// Duplicates
+
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     int nums[] = {3, 1, 3, 4, 2};
+//     int slow = nums[0], fast = nums[0];
+
+//         do {
+//             slow = nums[slow];
+//             fast = nums[nums[fast]];
+//         } while (slow != fast);
+
+//         slow = nums[0];
+//         while (slow != fast) {
+//             slow = nums[slow];
+//             fast = nums[fast];
+//         }
+
+//         cout << slow;
+// }
