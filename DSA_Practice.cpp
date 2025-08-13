0a39cbf54a89f54f21b21f4994e1a6e6ac5bbc8b
Hey_Nerd <gargharshit765@gmail.com>
2025-08-13 23:20:11 +0530

    Majority Element Practice

--- DSA_Practice.cpp
+++ DSA_Practice.cpp
@@ -3726,14 +3726,62 @@ using namespace std;
 
 
 
-int main(){
-    int arr[] = {2,2,4,6,6,7};
-    int n = sizeof(arr)/sizeof(arr[0]);
-    int l = 0, r = 5;
-    while(l < r){
-        int mid = l + (r - l) / 2;
-        if(arr[mid] == arr[mid + 1] || arr[mid] == arr[mid - 1]){
-            cout << arr[mid] << " ";
-        }
-    }
-}
+// int main(){
+//     int arr[] = {2,2,4,6,6,7};
+//     int n = sizeof(arr)/sizeof(arr[0]);
+//     int l = 0, r = 5;
+//     while(l < r){
+//         int mid = l + (r - l) / 2;
+//         if(arr[mid] == arr[mid + 1] || arr[mid] == arr[mid - 1]){
+//             cout << arr[mid] << " ";
+//         }
+//     }
+// }
+
+
+
+
+
+
+
+
+// // Majority element 2
+
+// class Solution {
+// public:
+//     vector<int> majorityElement(vector<int>& nums) {
+//         int n = nums.size();
+//         int cand1 = 0, cand2 = 0, count1 = 0, count2 = 0;
+
+//         // Candidate Selection
+//         for (int val : nums) {
+//             if (val == cand1) {
+//                 count1++;
+//             } else if (val == cand2) {
+//                 count2++;
+//             } else if (count1 == 0) {
+//                 cand1 = val;
+//                 count1 = 1;
+//             } else if (count2 == 0) {
+//                 cand2 = val;
+//                 count2 = 1;
+//             } else {
+//                 count1--;
+//                 count2--;
+//             }
+//         }
+
+//         // Validation
+//         count1 = count2 = 0;
+//         for (int val : nums) {
+//             if (val == cand1) count1++;
+//             else if (val == cand2) count2++;
+//         }
+
+//         vector<int> result;
+//         if (count1 > n / 3) result.push_back(cand1);
+//         if (count2 > n / 3) result.push_back(cand2);
+
+//         return result;
+//     }
+// };
