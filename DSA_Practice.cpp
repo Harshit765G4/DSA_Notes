3dc38f399e78165279de3776abffd02e6d7be018
Hey_Nerd <gargharshit765@gmail.com>
2025-08-19 14:15:51 +0530

    fix

--- DSA_Practice.cpp
+++ DSA_Practice.cpp
@@ -3897,32 +3897,32 @@
 
 
 
-class Solution {
-public:
-    int trap(vector<int>& arr) {
-        int sz = arr.size();
+// class Solution {
+// public:
+//     int trap(vector<int>& arr) {
+//         int sz = arr.size();
 
-        int left = 0 , right = sz - 1;
-        int leftMax = 0, rightMax = 0;
-        int totalWater = 0;
+//         int left = 0 , right = sz - 1;
+//         int leftMax = 0, rightMax = 0;
+//         int totalWater = 0;
 
-        while(left < right){
-            if(arr[left] < arr[right]){
-                if(arr[left] >= leftMax) {
-                    leftMax = arr[left];
-                } else {
-                    totalWater += leftMax - arr[left];
-                }
-                left++;
-            } else {
-                if(arr[right] >= rightMax) {
-                    rightMax = arr[right];
-                } else {
-                    totalWater += rightMax - arr[right];
-                }
-                right--;
-            }
-        }
-        return totalWater; 
-    }
-};
+//         while(left < right){
+//             if(arr[left] < arr[right]){
+//                 if(arr[left] >= leftMax) {
+//                     leftMax = arr[left];
+//                 } else {
+//                     totalWater += leftMax - arr[left];
+//                 }
+//                 left++;
+//             } else {
+//                 if(arr[right] >= rightMax) {
+//                     rightMax = arr[right];
+//                 } else {
+//                     totalWater += rightMax - arr[right];
+//                 }
+//                 right--;
+//             }
+//         }
+//         return totalWater; 
+//     }
+// };
