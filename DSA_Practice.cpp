b66aad28082c7653f4f8a74d5148747c9467a63d
Hey_Nerd <gargharshit765@gmail.com>
2025-08-19 23:14:30 +0530

    Print all subarrays

--- DSA_Practice.cpp
+++ DSA_Practice.cpp
@@ -3947,3 +3947,29 @@
 //     }
 //     return 0;
 // }
+
+
+
+
+
+
+// // Print all subarrays
+
+// #include <iostream>
+// using namespace std;
+
+// int main(){
+//     int arr[] = {1, 2, 3};
+//     int n = sizeof(arr) / sizeof(arr[0]);
+
+//     for(int start = 0; start < n; start++){
+//         for(int end = start; end < n; end++){
+//             // print subarray from start to end
+//             for(int k = start; k <= end; k++){
+//                 cout << arr[k] << " ";
+//             }
+//             cout << endl;
+//         }
+//     }
+//     return 0;
+// }
