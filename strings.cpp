22b3bea0f80a30fdff0fa62d62c482f967653c07
Hey_Nerd <gargharshit765@gmail.com>
2025-08-28 23:46:58 +0530

    strings

--- strings.cpp
+++ strings.cpp
@@ -1,8 +1,20 @@
 #include<iostream>
 using namespace std;
 
+// int main(){
+//     string str = "Hello, World!";
+//     cout << str << endl;
+//     return 0;
+// }
+
+
+
+
+
 int main(){
-    string str = "Hello, World!";
-    cout << str << endl;
+    string str1 = "Hello";
+    string str2 = "World";
+    string str3 = str1 + ", " + str2 + "!";
+    cout << str3 << endl; // Output: Hello, World!
     return 0;
 }
